25de934b8caffc942eada15fa5031245e463a8f5
Imran Bohoran <imran.bohoran@digital.cabinet-office.gov.uk>
2017-01-15 18:50:30 +0000

    Implementing the `transform ()` function.
     * Takes a list and replaces all `0`s with `(1 2 3 4 5 6 7 8 9)`.
     * All non-zero values are left as they are.
     * Test case to check the functionality and to make sur the return
       type is correct.

--- sudoku-solver-test.rkt
+++ sudoku-solver-test.rkt
@@ -11,6 +11,20 @@
 
    (check-equal? (atom? 2) #t "Should be an atom")
    (check-equal? (atom? '(2 3 4)) #f "Should not be an atom. It's a list")
+
+   (test-case
+    "List has length 4 and all elements even"
+    (let ([lst '(2 4 6 8)])
+      (check = (length lst) 4)
+      (for-each
+        (lambda (elt)
+          (check-pred even? elt))
+      lst)))
+   (test-case
+    "0s should be replaced"
+    (let ([matrix '(2 3 4 0 5)])
+      (check-equal? (transform matrix) '(2 3 4 '(1 2 3 4 5 6 7 8 9) 5)
+   )))
   )
 )
 
--- sudoku-solver.rkt
+++ sudoku-solver.rkt
@@ -5,13 +5,18 @@
 (define (atom? x)
   (not (pair? x)))
 
+(define (transform-rule x)
+  (if (zero? x)
+      (list 1 2 3 4 5 6 7 8 9)
+      x
+  ))
+
 (define (transform matrix)
-  "Not yet Implemented"
-)
+  (map transform-rule matrix))
 
 (define (solve matrix)
   "Not yet Implemented"
 )
 
 (provide atom?)
-
+(provide transform)
